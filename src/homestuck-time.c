a901070c058eee442a7634efa110bed2843fa96e
Wendel Scardua <wendelscardua@gmail.com>
2013-04-19 23:31:06 -0300

    mix with example_src to get started

--- src/homestuck-time.c
+++ src/homestuck-time.c
@@ -5,25 +5,41 @@
 
 #define MY_UUID { 0xDD, 0xA8, 0x71, 0x95, 0x63, 0x86, 0x40, 0xE7, 0x90, 0x19, 0x6C, 0xD8, 0x98, 0xC2, 0xAB, 0xC5 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+			 "Homestuck Time", "Wendel Scardua",
+			 1, 0, /* App version */
+			 RESOURCE_ID_IMAGE_MENU_ICON,
+			 APP_INFO_WATCH_FACE);
 
-Window window;
+Window s_window;
+TextLayer s_text;
 
+#define TIME_STR_BUFFER_BYTES 32
+char s_time_str_buffer[TIME_STR_BUFFER_BYTES];
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+void handle_tick(AppContextRef ctx, PebbleTickEvent *event) {
+    string_format_time(s_time_str_buffer, TIME_STR_BUFFER_BYTES, "%I:%M:%S %p", event->tick_time);
+    text_layer_set_text(&s_text, s_time_str_buffer);
 }
 
+void handle_init(AppContextRef ctx) {
+    (void)ctx;
+
+    window_init(&s_window, "Fenestrated Plane");
+    window_stack_push(&s_window, true /* Animated */);
+
+    text_layer_init(&s_text, s_window.layer.frame);
+    strcpy(s_time_str_buffer, "");
+    text_layer_set_text(&s_text, s_time_str_buffer);
+    layer_add_child(&s_window.layer, &s_text.layer);
+}
 
 void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
-  };
-  app_event_loop(params, &handlers);
+    PebbleAppHandlers handlers = {
+        .init_handler = &handle_init,
+        .tick_info = {
+            .tick_handler = &handle_tick,
+            .tick_units = SECOND_UNIT
+        }
+    };
+    app_event_loop(params, &handlers);
 }
