2a3b68dbb54ee8a899d21a991b50bf63d3b7f54f
Wendel Scardua <wendelscardua@gmail.com>
2013-04-20 01:09:47 -0300

    adds time symbol bg

--- src/homestuck-time.c
+++ src/homestuck-time.c
@@ -13,10 +13,38 @@ PBL_APP_INFO(MY_UUID,
 Window window;
 TextLayer text_layer;
 
+BmpContainer background_image;
+
+const int BACKGROUND_RESOURCE_IDS[] = {
+	RESOURCE_ID_IMAGE_BACKGROUND_0,
+	RESOURCE_ID_IMAGE_BACKGROUND_1,
+	RESOURCE_ID_IMAGE_BACKGROUND_2,
+	RESOURCE_ID_IMAGE_BACKGROUND_3,
+};
+
+static int bg_count = 0;
+
 #define TIME_STR_BUFFER_BYTES 32
 char time_text_buffer[TIME_STR_BUFFER_BYTES];
 
+void set_container_image(BmpContainer *bmp_container, const int resource_id, GPoint origin) {
+
+	layer_remove_from_parent(&bmp_container->layer.layer);
+	bmp_deinit_container(bmp_container);
+
+	bmp_init_container(resource_id, bmp_container);
+
+	GRect frame = layer_get_frame(&bmp_container->layer.layer);
+	frame.origin.x = origin.x;
+	frame.origin.y = origin.y;
+	layer_set_frame(&bmp_container->layer.layer, frame);
+
+	layer_add_child(&window.layer, &bmp_container->layer.layer);
+}
+
 void handle_tick(AppContextRef ctx, PebbleTickEvent *event) {
+	set_container_image(&background_image, BACKGROUND_RESOURCE_IDS[bg_count], GPoint(0, 0));
+	bg_count = (bg_count + 1) % 4;
     string_format_time(time_text_buffer, TIME_STR_BUFFER_BYTES, "%H:%M:%S", event->tick_time);
     text_layer_set_text(&text_layer, time_text_buffer);
 }
@@ -27,6 +55,11 @@ void handle_init(AppContextRef ctx) {
     window_init(&window, "Fenestrated Plane");
     window_stack_push(&window, true /* Animated */);
 
+	// background time symbol
+	bmp_init_container(BACKGROUND_RESOURCE_IDS[3], &background_image);
+    layer_add_child(&window.layer, &background_image.layer.layer);
+
+
     text_layer_init(&text_layer, window.layer.frame);
 	text_layer_set_font(&text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_color(&text_layer, GColorWhite);
@@ -36,9 +69,17 @@ void handle_init(AppContextRef ctx) {
     layer_add_child(&window.layer, &text_layer.layer);
 }
 
+
+void handle_deinit(AppContextRef ctx) {
+	(void)ctx;
+
+    bmp_deinit_container(&background_image);
+
+}
 void pbl_main(void *params) {
     PebbleAppHandlers handlers = {
         .init_handler = &handle_init,
+	    .deinit_handler = &handle_deinit,
         .tick_info = {
             .tick_handler = &handle_tick,
             .tick_units = SECOND_UNIT
